41d42e1b27c535f6f2a9f892ef4b91a92efc1909
nader777 <naderfekry3@gmail.com>
2024-05-03 15:08:06 -0700

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,18 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+# Sets up webservers for deployment: (Run script on both servers)
 
-apt-get update
-apt-get install -y nginx
+WEBSTATIC="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
-service nginx restart
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+#run as root and make the two main dir
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+#create index.html and echo text for testing
+echo "This tests index.html on Nginx config" | sudo tee /data/web_static/releases/test/index.html
+#create symbolic link(shortcut file)
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+#Give ownership of the /data/ folder to the ubuntu user
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i "35i $WEBSTATIC" /etc/nginx/sites-available/default
+sudo service nginx start
