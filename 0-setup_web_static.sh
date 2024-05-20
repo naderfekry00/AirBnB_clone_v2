061b9bed603663dee3802287c230a0fae13a9a02
nader <naderfekry3@gmail.com>
2024-05-19 19:40:14 -0700

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
-# Sets up webservers for deployment: (Run script on both servers)
-
-WEBSTATIC="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
-
+# script that sets up web servers for the deployment of web_static
 sudo apt-get update
-sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-#run as root and make the two main dir
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-#create index.html and echo text for testing
-echo "This tests index.html on Nginx config" | sudo tee /data/web_static/releases/test/index.html
-#create symbolic link(shortcut file)
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-#Give ownership of the /data/ folder to the ubuntu user
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i "35i $WEBSTATIC" /etc/nginx/sites-available/default
-sudo service nginx start
+sudo ufw allow 'Nginx HTTP'
+
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
+sudo service nginx restart
